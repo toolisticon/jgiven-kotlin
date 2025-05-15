ff87ee5bb8b3e88fd39da175813823cabd6b92fb
Jan Galinski <jan.galinski@holisticon.de>
2025-05-15 19:27:39 +0200

    support receiver style syntax, fixes #213

--- core/src/main/kotlin/io/toolisticon/testing/jgiven/JGivenKotlin.kt
+++ core/src/main/kotlin/io/toolisticon/testing/jgiven/JGivenKotlin.kt
@@ -1,4 +1,5 @@
 @file:Suppress("unused")
+
 package io.toolisticon.testing.jgiven
 
 import com.tngtech.jgiven.Stage
@@ -10,14 +11,29 @@ import com.tngtech.jgiven.base.ScenarioTestBase
 val <G : Stage<G>, W : Stage<W>, T : Stage<T>> ScenarioTestBase<G, W, T>.GIVEN: G get() = given()
 
 /**
+ * Use `GIVEN` instead of `given()` on Given-Stage "G", allowing you to write `GIVEN { ... }`
+ */
+fun <G : Stage<G>, W : Stage<W>, T : Stage<T>> ScenarioTestBase<G, W, T>.GIVEN(block: G.() -> G): G = GIVEN.block()
+
+/**
  * Use `WHEN` instead of `when()` on When-Stage "W"
  */
 val <G : Stage<G>, W : Stage<W>, T : Stage<T>> ScenarioTestBase<G, W, T>.WHEN: W get() = `when`()
 
 /**
+ * Use `WHEN` instead of `when()` on When-Stage "W", allowing you to write `WHEN { ... }`
+ */
+fun <G : Stage<G>, W : Stage<W>, T : Stage<T>> ScenarioTestBase<G, W, T>.WHEN(block: W.() -> W): W = WHEN.block()
+
+/**
  * Use `THEN` instead of `then()` on Then-Stage "T"
  */
 val <G : Stage<G>, W : Stage<W>, T : Stage<T>> ScenarioTestBase<G, W, T>.THEN: T get() = then()
+
+/**
+ * Use `THEN` instead of `then()` on Then-Stage "T", allowing you to write `THEN { ... }`
+ */
+fun <G : Stage<G>, W : Stage<W>, T : Stage<T>> ScenarioTestBase<G, W, T>.THEN(block: T.() -> T): T = THEN.block()
 
 /**
  * Use `AND` instead of `and()`
@@ -51,4 +67,4 @@ val <X : Stage<X>> Stage<X>.self: X get() = self()!!
  * `step` is short for `self().apply {}` and returns the Stage instance for fluent usage.
  *
  */
-inline fun  <X : Stage<X>> Stage<X>.step(block: X.() -> Unit) = self.apply(block)
+inline fun <X : Stage<X>> Stage<X>.step(block: X.() -> Unit) = self.apply(block)
